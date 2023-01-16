373e4d4d659fb9a1bbdaa20fcd761a270a0472f9
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2023-01-16 16:42:16 -0500

    responsive layout added

--- my-project/pages/index.tsx
+++ my-project/pages/index.tsx
@@ -13,6 +13,12 @@ import deved from "../public/dev-ed-wave.png";
 import design from "../public/design.png";
 import code from "../public/code.png";
 import consulting from "../public/consulting.png";
+import web1 from "../public/web1.png";
+import web2 from "../public/web2.png";
+import web3 from "../public/web3.png";
+import web4 from "../public/web4.png";
+import web5 from "../public/web5.png";
+import web6 from "../public/web6.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,7 +32,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="bg-white px-10">
+      <main className="bg-white px-10 md:px-20 lg:px-40">
         <section className=" min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
             <h1 className="text-xl font-burtons">Ameen Mohiyuddin</h1>
@@ -46,11 +52,11 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p-10">
-            <h2 className="text-5xl py-2 text-teal-600 font-medium">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl">
               Ameen Mohiyuddin
             </h2>
-            <h3 className="text-2xl py-2">Developer and Designer.</h3>
-            <p className="text-md py-5 leading-8 text-gray-800">
+            <h3 className="text-2xl py-2 md:text-3xl">Developer and Designer.</h3>
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">
               Freelancer providing services for programming and design content
               needs. Join me down below and let's get cracking!
             </p>
@@ -66,30 +72,42 @@ export default function Home() {
               <AiFillYoutube className="cursor-pointer text-5xl" />
             </a>
           </div>
-          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
-            <Image src={deved} fill style={{ objectFit: "cover" }} alt="image af a man waving"/>
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
+            <Image
+              src={deved}
+              fill
+              style={{ objectFit: "cover" }}
+              alt="image af a man waving"
+            />
           </div>
         </section>
 
         <section>
           <div>
-            <h3 className="text-3xl py-1">Services I offer</h3>
-            <p className="text-md py-2 leading-8 text-gray-800">
-              Since the biginning of my journey as a freelance developer, I've
+            <h3 className="text-3xl py-1 md:text-3xl max-w-lg mx-auto">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">
+              Since the beginning of my journey as a freelance developer, I've
               done remote work for
               <span className="text-teal-500"> agencies </span> consulted
               <span className="text-teal-500"> startups </span> and collaborated
               with talented people to create digital products for both business
               and consumer use.
             </p>
-            <p className="text-md py-2 leading-8 text-gray-800">
+            <p className="text-md py-2 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">
               I offer from a wide range of services, including brand design,
               programming and teaching
             </p>
           </div>
-          <div>
+          <div className="lg:flex gap-10">
+            {/* card - design */}
             <div className="text-center shadow-lg p-10 rounded-xl my-10">
-              <Image src={design} width={100} height={100} alt="3D design logo" className="mx-auto" />
+              <Image
+                src={design}
+                width={100}
+                height={100}
+                alt="3D design logo"
+                className="mx-auto"
+              />
               <h3 className="text-lg font-medium pt-8 pb-2">
                 Beautiful Designs
               </h3>
@@ -101,6 +119,86 @@ export default function Home() {
               <p className="text-gray-800 py-1">Photoshop</p>
               <p className="text-gray-800 py-1">Illustrator</p>
               <p className="text-gray-800 py-1">Figma</p>
+            </div>
+
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image
+                src={code}
+                width={100}
+                height={100}
+                alt="3D design logo"
+                className="mx-auto"
+              />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">
+                Creating elegant designs suited for your needs following core
+                design theory.
+              </p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+
+            <div className="text-center shadow-lg p-10 rounded-xl my-10">
+              <Image
+                src={consulting}
+                width={100}
+                height={100}
+                alt="3D design logo"
+                className="mx-auto"
+              />
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Beautiful Designs
+              </h3>
+              <p className="py-2">
+                Creating elegant designs suited for your needs following core
+                design theory.
+              </p>
+              <h4 className="py-4 text-teal-600">Design tools I use</h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <h3 className="text-3xl py-1 md:text-4xl max-w-lg mx-auto">Portfolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">
+              Since the beginning of my journey as a freelance developer, I've
+              done remote work for
+              <span className="text-teal-500"> agencies </span> consulted
+              <span className="text-teal-500"> startups </span> and collaborated
+              with talented people to create digital products for both business
+              and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto">
+              I offer from a wide range of services, including brand design,
+              programming and teaching
+            </p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1">
+              <Image src={web1} alt="" className="rounded-lg object-cover" width={"100%"} height={"100%"} style={{ objectFit: "cover" }}/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web2} alt="" className="rounded-lg object-cover" width={"100%"} height={"100%"} style={{ objectFit: "cover" }}/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web3} alt="" className="rounded-lg object-cover" width={"100%"} height={"100%"} style={{ objectFit: "cover" }}/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web4} alt="" className="rounded-lg object-cover" width={"100%"} height={"100%"} style={{ objectFit: "cover" }}/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web5} alt="" className="rounded-lg object-cover" width={"100%"} height={"100%"} style={{ objectFit: "cover" }}/>
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image src={web6} alt="" className="rounded-lg object-cover" width={"100%"} height={"100%"} style={{ objectFit: "cover" }}/>
             </div>
           </div>
         </section>
