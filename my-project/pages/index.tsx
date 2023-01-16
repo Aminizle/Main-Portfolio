542ca1facf7de0ff03a273be000c8023daf6de2a
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2023-01-15 23:17:54 -0500

    added style

--- my-project/pages/index.tsx
+++ my-project/pages/index.tsx
@@ -19,10 +19,10 @@ export default function Home() {
       <main className="bg-black px-10">
         <section className=" min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-xl">Ameen Mohiyuddin</h1>
+            <h1 className="text-white text-xl font-burtons">Ameen Mohiyuddin</h1>
             <ul className="flex items-center">
               <li>
-                <BsFillMoonStarsFill className="cursor-pointer text-2xl"/>
+                <BsFillMoonStarsFill className="text-white cursor-pointer text-2xl"/>
               </li>
               <li>
                 <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-wide px-4 py-2 rounded-md ml-8" href="#">Resume</a>
@@ -35,3 +35,6 @@ export default function Home() {
     </>
   );
 }
+
+
+// 21:20
