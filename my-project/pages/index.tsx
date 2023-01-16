4ff28ee24a69bbe0fa41677c0d054ba320b3a109
Ameen Mohiyuddin <57292718+Aminizle@users.noreply.github.com>
2023-01-16 10:47:00 -0500

    added content

--- my-project/pages/index.tsx
+++ my-project/pages/index.tsx
@@ -3,6 +3,16 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import styles from "@/styles/Home.module.css";
 import { BsFillMoonStarsFill } from "react-icons/bs";
+import {
+  AiFillTwitterCircle,
+  AiFillLinkedin,
+  AiFillYoutube,
+} from "react-icons/ai";
+import image from "next/image";
+import deved from "../public/dev-ed-wave.png";
+import design from "../public/design.png";
+import code from "../public/code.png";
+import consulting from "../public/consulting.png";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,22 +29,84 @@ export default function Home() {
       <main className="bg-black px-10">
         <section className=" min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
-            <h1 className="text-white text-xl font-burtons">Ameen Mohiyuddin</h1>
+            <h1 className="text-xl font-burtons">Ameen Mohiyuddin</h1>
             <ul className="flex items-center">
               <li>
-                <BsFillMoonStarsFill className="text-white cursor-pointer text-2xl"/>
+                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
               </li>
               <li>
-                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-wide px-4 py-2 rounded-md ml-8" href="#">Resume</a>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-wide px-4 py-2 rounded-md ml-8"
+                  href="#"
+                >
+                  Resume
+                </a>
               </li>
               <li></li>
             </ul>
           </nav>
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium">
+              Ameen Mohiyuddin
+            </h2>
+            <h3 className="text-2xl py-2">Developer and Designer.</h3>
+            <p className="text-md py-5 leading-8 text-gray-800">
+              Freelancer providing services for programming and design content
+              needs. Join me down below and let's get cracking!
+            </p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <a href="#">
+              <AiFillTwitterCircle className="cursor-pointer text-5xl" />
+            </a>
+            <a href="#">
+              <AiFillLinkedin className=" cursor-pointer text-5xl" />
+            </a>
+            <a href="#">
+              <AiFillYoutube className="cursor-pointer text-5xl" />
+            </a>
+          </div>
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden">
+            <Image src={deved} fill style={{ objectFit: "cover" }} />
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <h3 className="text-3xl py-1">Services I offer</h3>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              Since the biginning of my journey as a freelance developer, I've
+              done remote work for
+              <span className="text-teal-500"> agencies </span> consulted
+              <span className="text-teal-500"> startups </span> and collaborated
+              with talented people to create digital products for both business
+              and consumer use.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800">
+              I offer from a wide range of services, including brand design,
+              programming and teaching
+            </p>
+          </div>
+          <div>
+            <div>
+              <Image src={design} width={100} height={100} />
+              <h3 className="text-lg font-medium pt-8 pb-2">Beautiful Designs</h3>
+              <p className="py-2">
+                Creating elegant designs suited for your needs following core
+                design theory.
+              </p>
+              <h4 className="py-4 text-teal-600">
+                Design tools I use
+              </h4>
+              <p className="text-gray-800 py-1">Photoshop</p>
+              <p className="text-gray-800 py-1">Illustrator</p>
+              <p className="text-gray-800 py-1">Figma</p>
+            </div>
+          </div>
         </section>
       </main>
     </>
   );
 }
 
-
-// 21:20
+// font is not working
